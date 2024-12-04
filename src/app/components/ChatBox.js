86d0f065d41360781efd7259cf9441b6ae54f6e8
Jill-Vadsola <jvadsola@student.concordia.ab.ca>
2024-12-03 17:52:29 -0700

    Chat Box UI changes

--- src/app/components/ChatBox.js
+++ src/app/components/ChatBox.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { auth, db } from "../config/firebaseConfig";
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 import Message from "./Message";
-import { times } from "lodash";
+import SendIcon from "@mui/icons-material/Send";
 
 export default function ChatBox({ ChatRoomId }) {
   const [chats, setChats] = useState([]);
@@ -62,8 +62,8 @@ export default function ChatBox({ ChatRoomId }) {
             ))
           : null}
       </Grid2>
-      <Grid2 size={12} display={"flex"} alignItems={"center"} gap={1}>
-        <Grid2 item xs={9} sm={9} md={9} bgcolor={"wheat"} borderRadius={2}>
+      <Grid2 display={"flex"} alignItems={"center"} gap={1}>
+        <Grid2 item xs={10} sm={10} md={10} bgcolor={"wheat"} borderRadius={2}>
           <TextField
             size="small"
             label="Message"
@@ -75,10 +75,21 @@ export default function ChatBox({ ChatRoomId }) {
             }}
           />
         </Grid2>
-        <Grid2 item xs={3} sm={3} md={3}>
-          <Button variant="contained" color="primary" onClick={sendMessage}>
-            Send
-          </Button>
+        <Grid2
+          item
+          xs={2}
+          sm={2}
+          md={2}
+          borderRadius={2}
+          p={1}
+          sx={{ cursor: "pointer" }}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          bgcolor={"wheat"}
+          onClick={sendMessage}
+        >
+          <SendIcon />
         </Grid2>
       </Grid2>
     </Grid2>
